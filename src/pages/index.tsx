e9ce386f690a599bce60793e42b0cb74ed3295ef
Denys Soloviov <densolwork@gmail.com>
2024-02-08 18:02:45 +0200

    added logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,135 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { candidatesData } from "@/common/candidates-mock";
+import { useCallback, useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+interface WorkHistoryInterface {
+  company: string;
+  title: string;
+  startDate: string;
+  endDate: string;
+}
+
+interface CandidateInterface {
+  firstName: string;
+  lastName: string;
+  location: string;
+  workHistory: WorkHistoryInterface[];
+}
 
 export default function Home() {
+  const [query, setQuery] = useState("");
+  const [candidates, setCandidates] = useState<CandidateInterface[]>([]);
+  const [selectedCandidates, setSelectedCandidates] = useState<
+    CandidateInterface[]
+  >([]);
+
+  const debounce = (callback: Function, delay: number) => {
+    // type it
+    let timer: any;
+
+    return function (...args: any) {
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    };
+  };
+
+  const getData = (query: string): Promise<CandidateInterface[]> =>
+    new Promise((reslove) => {
+      setTimeout(() => {
+        const filteredData = candidatesData.filter((candidate) => {
+          const fullName = `${candidate.firstName} ${candidate.lastName}`;
+
+          return fullName.toLowerCase().includes(query.toLowerCase());
+        });
+
+        reslove(filteredData);
+      }, 250);
+    });
+
+  const debouncedGetData = useCallback(
+    debounce(async (query: string) => {
+      const data = await getData(query);
+
+      console.log(query);
+
+      const filteredData = data.filter(
+        (candidate) => !selectedCandidates.includes(candidate)
+      );
+
+      setCandidates(filteredData);
+    }, 150),
+    [selectedCandidates]
+  );
+
+  const handleChange = (query: string) => {
+    setQuery(query);
+
+    if (!!query) {
+      debouncedGetData(query);
+    } else {
+      setCandidates([]);
+    }
+  };
+
+  const handleAddCandidate = (candidate: CandidateInterface) => {
+    setSelectedCandidates((prevCandidates) => [...prevCandidates, candidate]);
+
+    setCandidates([]);
+    setQuery("");
+  };
+
+  // const calculateDurationAndSort = (workHistories: WorkHistoryInterface[]) => {
+  //   const reducedWorkHistory = workHistories.reduce((acc, history) => {
+  //     const start = new Date(history.startDate);
+  //     const end = new Date(history.endDate);
+  //     const duration = end.getDate() - start.getDate();
+  //   }, []);
+  // };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={`w-full p-16`}>
+      <div className={`mb-10`}>
+        <input
+          onChange={(e) => handleChange(e.currentTarget.value)}
+          value={query}
+          className={`bg-gray-500 w-full`}
+          type="text"
+        />
+
+        <div>
+          {!!candidates?.length &&
+            candidates.map((candidate, index) => (
+              <div
+                onClick={() => handleAddCandidate(candidate)}
+                key={index}
+              >{`${candidate.firstName} ${candidate.lastName}`}</div>
+            ))}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div>
+        <h3>Selected Candidates</h3>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div>
+          {!!selectedCandidates?.length &&
+            selectedCandidates.map((candidate, index) => (
+              <div key={index}>
+                <h2>{`${candidate.firstName} ${candidate.lastName}`}</h2>
+                <h3>{candidate.location}</h3>
+                <div>
+                  {candidate.workHistory.map((history, index) => (
+                    <div key={index}>
+                      <p>{history.title}</p>
+                      <p>{history.company}</p>
+                      <p>{history.company}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </main>
   );
